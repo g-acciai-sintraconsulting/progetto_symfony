1f55e1b801f7fcf834b45ab2f04a235146549b9c
Francesca Tani <f.tani@sintraconsulting.eu>
2021-12-29 15:29:30 +0100

    navigazione da visual

--- agenda2/src/Controller/VisualController.php
+++ agenda2/src/Controller/VisualController.php
@@ -34,18 +34,126 @@ class VisualController extends AbstractController
                     <script src="https://cdn.syncfusion.com/ej2/ej2-lists/dist/global/ej2-lists.min.js" type="text/javascript"></script>
                     <script src="https://cdn.syncfusion.com/ej2/ej2-popups/dist/global/ej2-popups.min.js" type="text/javascript"></script>
                     <script src="https://cdn.syncfusion.com/ej2/ej2-calendars/dist/global/ej2-calendars.min.js" type="text/javascript"></script>
-               </head>
+                    </head>
           <body>
             <!-- Add the HTML <div> element  -->
              <div id="element"/>
             <script>
                 // initialize the Calendar component
-                var calendar = new ej.calendars.Calendar();
+                var calendar = new ej.calendars.Calendar({
+                    change:valueChange
+                });
          
                 // Render the initialized button.
-                calendar.appendTo('#element')
+                calendar.appendTo('#element');
+                
+                Date.prototype.addHours = function(h) {
+                    this.setTime(this.getTime() + (h*60*60*1000));
+                    return this;
+                  }
+
+                function valueChange(args){
+                    var url='/event?date=' + args.value.addHours(12).toISOString();
+                    window.location.href=url;  
+                } 
+                 
             </script>
           </body>
+
+
+          <div class="col-lg-12 control-section">
+          <div class="control_wrapper">
+              <div id="container" style="overflow:auto">
+                  <div id="calendar" class="e-customStyle"></div>
+                  <br />
+                  <span id="date_label"> Selected Value: </span>
+              </div>
+          </div>
+          </div>
+        <style>
+      
+          .e-customStyle span.e-icons.highlight {
+              margin-top: -13px;
+              display: block;
+              margin-left: 4px;
+          }
+      
+          .e-customStyle span.e-icons.highlight,
+          .e-customStyle span.e-icons.highlight:before {
+              color: rgb(0, 0, 255);
+          }
+      
+          .highcontrast .e-customStyle span.e-icons.highlight,
+          .highcontrast .e-customStyle span.e-icons.highlight:before,
+          .material-dark .e-customStyle span.e-icons.highlight,
+          .material-dark .e-customStyle span.e-icons.highlight:before,
+          .fabric-dark .e-customStyle span.e-icons.highlight,
+          .fabric-dark .e-customStyle span.e-icons.highlight:before,
+          .bootstrap-dark .e-customStyle span.e-icons.highlight,
+          .bootstrap-dark .e-customStyle span.e-icons.highlight:before,
+          .bootstrap5-dark .e-customStyle span.e-icons.highlight,
+          .bootstrap5-dark .e-customStyle span.e-icons.highlight:before,
+          .tailwind-dark .e-customStyle span.e-icons.highlight,
+          .tailwind-dark .e-customStyle span.e-icons.highlight:before {
+              color: #ff7500;
+          }
+      
+          .e-customStyle .e-other-month span.e-icons.highlight:before {
+              content: "";
+          }
+      
+          body.highcontrast.e-bigger .e-customStyle.e-calendar span.e-icons.highlight:before,
+          body.highcontrast .e-customStyle.e-bigger.e-calendar span.e-icons.highlight:before,
+          body.bootstrap.e-bigger .e-customStyle.e-calendar span.e-icons.highlight:before,
+          body.bootstrap .e-customStyle.e-bigger.e-calendar span.e-icons.highlight:before {
+              top: 1px;
+          }
+      
+          .e-bigger .e-customStyle.e-calendar span.e-icons.highlight:before,
+          .e-customStyle.e-bigger.e-calendar span.e-icons.highlight:before {
+              top: -1px;
+          }
+      
+          .e-customStyle span.e-icons.highlight:before {
+              content: "\e190";
+              vertical-align: middle;
+              margin-right: 3px;
+              font-size: 4px;
+              position: relative;
+              top: 1px;
+              font-weight: normal;
+          }
+      
+          .e-customStyle .e-selected span.e-icons.highlight:before {
+              color: #fff;
+          }
+      
+          .e-customStyle span.e-icons.highlight,
+          .e-customStyle.e-calendar .e-content span.special,
+          .e-customStyle.e-calendar .e-content span.daycell {
+              font-weight: bold;
+          }
+      
+          body.highcontrast #date_label {
+              color: white;
+          }
+      
+          body.tailwind #date_label {
+              width: 265px;
+          }
+      
+      
+          /* end of custom generated icons styles */
+        </style>
+        <div class="col-lg-12 control-section">
+    <div class="control_wrapper">
+        <div id="container" style="overflow:auto">
+            <div id="calendar"></div><br />
+            </div>
+        </div>
+    </div>
+
+
         </html>
     EOF
         );
